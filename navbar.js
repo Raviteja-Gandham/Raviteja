17962c88be6f2d77af18d0bbf0deffa782d09625
Raviteja-Gandham <168612928+Raviteja-Gandham@users.noreply.github.com>
2024-08-05 22:38:47 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,19 @@
+
+
+
+document.getElementById('scroll_contact').addEventListener('click', function() {
+        document.getElementById('target_contact').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById('scroll_about').addEventListener('click', function() {
+        document.getElementById('target_about').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById('scroll_mission').addEventListener('click', function() {
+        document.getElementById('target_mission').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById('scroll_service').addEventListener('click', function() {
+        document.getElementById('target_service').scrollIntoView({ behavior: 'smooth' });
+    });
+   
